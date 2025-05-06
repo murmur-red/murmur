2b0678124c43041732665fad14dedee68da9778a
Yelena Ryzhova <yelenaryzhova@Yelenas-MacBook-Pro.local>
2025-05-06 21:58:30 +0200

    Initial commit

---
+++ src/pages/ComingSoon.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ComingSoon.css";
+
+const ComingSoon = () => {
+  return (
+    <div className="coming-soon-container">
+      <div className="grain-overlay"></div>
+      <h1 className="coming-soon-text">murmur coming soon</h1>
+    </div>
+  );
+};
+
+export default ComingSoon;
